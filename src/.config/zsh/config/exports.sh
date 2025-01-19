e2eb0c05a272e0d12362f810a6acf01312eb148f
Martin Sione <martinsione03@gmail.com>
2025-01-19 18:09:11 -0300

    add exports

--- src/.config/zsh/config/exports.sh
+++ src/.config/zsh/config/exports.sh
@@ -1,1 +1,8 @@
+# node
+export PATH=/home/martin/.fnm:$PATH
+eval "`fnm env`"
 
+## python
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+eval "$(pyenv init - zsh)"
--- src/.config/zsh/config/scripts.sh
+++ src/.config/zsh/config/scripts.sh
@@ -2,7 +2,7 @@ function find_projects() {
   local selected_dir=$(find ~/Developer \
     -type d \( -name node_modules -o -name build -o -name dist \) -prune \
     -o \( -name .git -prune -print \) \
-    | sed 's/\/\.git$//' | fzf)
+    | sed 's/\/\.git$//' | fzf --layout=reverse --height 40%)
   
   if [[ -n "$selected_dir" ]]; then
     cd "$selected_dir"
